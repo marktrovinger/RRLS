546405fe47ee917a195c2a6dfcce60e57b8d9967
Mark Trovinger <mark.trovinger@gmail.com>
2026-05-19 18:58:44 +0000

    updated variable names for joints for reach

--- rrls/envs/robotics_reach.py
+++ rrls/envs/robotics_reach.py
@@ -133,7 +133,7 @@ class RobustReachDense(Wrapper):
         if self.forearm_roll_link_mass is not None:
             self.unwrapped.model.body_mass[16] = forearm_roll_link_mass
 
-class ForceReach(Wrapper):
+class ForceReachDense(Wrapper):
     """
     Force Reach environment. You can apply forces to the robot using the env.data.xfrc_applied
     attribute. The parameters are:
@@ -151,12 +151,24 @@ class ForceReach(Wrapper):
         super().__init__(env = gym.make("FetchReachDense-v4", **kwargs))
         self.set_params()
     
-    # TODO:  correct variable names to the joints
     def set_params(
             self,
             shoulder_pan_joint: float | None = None,
+            shoulder_lift_joint: float | None = None,
+            upperarm_roll_joint: float | None = None,
+            elbow_flex_joint: float | None = None,
+            forearm_roll_joint: float | None = None,
+            wrist_flex_joint: float | None = None,
+            wrist_roll_joint: float | None = None,
+
     ):
         self.shoulder_pan_joint = shoulder_pan_joint
+        self.shoulder_lift_joint = shoulder_lift_joint
+        self.upperarm_roll_joint = upperarm_roll_joint
+        self.elbow_flex_joint = elbow_flex_joint
+        self.forearm_roll_joint = forearm_roll_joint
+        self.wrist_flex_joint = wrist_flex_joint
+        self.wrist_roll_joint = wrist_roll_joint
         self._change_params()
 
     def get_params(self):
@@ -179,10 +191,104 @@ class ForceReach(Wrapper):
     def _change_params(
             self,
             shoulder_pan_joint: float | None = None,
+            shoulder_lift_joint: float | None = None,
+            upperarm_roll_joint: float | None = None,
+            elbow_flex_joint: float | None = None,
+            forearm_roll_joint: float | None = None,
+            wrist_flex_joint: float | None = None,
+            wrist_roll_joint: float | None = None,
     ):
         if self.shoulder_pan_joint is not None:
             self.unwrapped.data.qfrc_applied[6] = shoulder_pan_joint  # type: ignore
-        # if self.upperarm_roll_link_y is not None:
-        #     self.unwrapped.data.xfrc_applied[14, 1] = upperarm_roll_link_y  # type: ignore
-        # if self.upperarm_roll_link_z is not None:
-        #     self.unwrapped.data.xfrc_applied[14, 2] = upperarm_roll_link_z  # type: ignore
+        if self.shoulder_lift_joint is not None:
+             self.unwrapped.data.qfrc_applied[7] = shoulder_lift_joint  # type: ignore
+        if self.upperarm_roll_joint is not None:
+             self.unwrapped.data.qfrc_applied[8] = upperarm_roll_joint  # type: ignore
+        if self.elbow_flex_joint is not None:
+             self.unwrapped.data.qfrc_applied[9] = elbow_flex_joint  # type: ignore
+        if self.forearm_roll_joint is not None:
+             self.unwrapped.data.qfrc_applied[10] = forearm_roll_joint  # type: ignore
+        if self.wrist_flex_joint is not None:
+             self.unwrapped.data.qfrc_applied[11] = wrist_flex_joint  # type: ignore
+        if self.wrist_roll_joint is not None:
+             self.unwrapped.data.qfrc_applied[12] = wrist_roll_joint  # type: ignore
+
+class ForceReach(Wrapper):
+    """
+    Force Reach environment. You can apply forces to the robot using the env.data.xfrc_applied
+    attribute. The parameters are:
+        - shoulder_pan_joint
+    """
+    metadata = {  # type: ignore
+        "render_modes": [
+            "human",
+            "rgb_array",
+            "depth_array",
+        ],
+    }
+
+    def __init__(self,  **kwargs: dict[str, Any]):
+        super().__init__(env = gym.make("FetchReach-v4", **kwargs))
+        self.set_params()
+    
+    def set_params(
+            self,
+            shoulder_pan_joint: float | None = None,
+            shoulder_lift_joint: float | None = None,
+            upperarm_roll_joint: float | None = None,
+            elbow_flex_joint: float | None = None,
+            forearm_roll_joint: float | None = None,
+            wrist_flex_joint: float | None = None,
+            wrist_roll_joint: float | None = None,
+
+    ):
+        self.shoulder_pan_joint = shoulder_pan_joint
+        self.shoulder_lift_joint = shoulder_lift_joint
+        self.upperarm_roll_joint = upperarm_roll_joint
+        self.elbow_flex_joint = elbow_flex_joint
+        self.forearm_roll_joint = forearm_roll_joint
+        self.wrist_flex_joint = wrist_flex_joint
+        self.wrist_roll_joint = wrist_roll_joint
+        self._change_params()
+
+    def get_params(self):
+        return{
+            "shoulder_pan_joint": self.shoulder_pan_joint,
+        }
+
+    def reset(self, *, seed: int | None = None, options: dict | None = None):
+        if options is not None:
+            self.set_params(**options)
+        obs, info = self.env.reset(seed=seed, options=options)
+        info.update(self.get_params())
+        return obs, info
+
+    def step(self, action):
+        obs, reward, terminated, truncated, info = self.env.step(action)
+        info.update(self.get_params())
+        return obs, reward, terminated, truncated, info
+    
+    def _change_params(
+            self,
+            shoulder_pan_joint: float | None = None,
+            shoulder_lift_joint: float | None = None,
+            upperarm_roll_joint: float | None = None,
+            elbow_flex_joint: float | None = None,
+            forearm_roll_joint: float | None = None,
+            wrist_flex_joint: float | None = None,
+            wrist_roll_joint: float | None = None,
+    ):
+        if self.shoulder_pan_joint is not None:
+            self.unwrapped.data.qfrc_applied[6] = shoulder_pan_joint  # type: ignore
+        if self.shoulder_lift_joint is not None:
+             self.unwrapped.data.qfrc_applied[7] = shoulder_lift_joint  # type: ignore
+        if self.upperarm_roll_joint is not None:
+             self.unwrapped.data.qfrc_applied[8] = upperarm_roll_joint  # type: ignore
+        if self.elbow_flex_joint is not None:
+             self.unwrapped.data.qfrc_applied[9] = elbow_flex_joint  # type: ignore
+        if self.forearm_roll_joint is not None:
+             self.unwrapped.data.qfrc_applied[10] = forearm_roll_joint  # type: ignore
+        if self.wrist_flex_joint is not None:
+             self.unwrapped.data.qfrc_applied[11] = wrist_flex_joint  # type: ignore
+        if self.wrist_roll_joint is not None:
+             self.unwrapped.data.qfrc_applied[12] = wrist_roll_joint  # type: ignore
